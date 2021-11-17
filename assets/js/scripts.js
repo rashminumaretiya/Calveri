3cde7179937cfb2216247d070d2b2a4e22ae43ed
rashminumaretiya <91937152+rashminumaretiya@users.noreply.github.com>
2021-11-17 14:29:35 +0530

    script update

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -439,7 +439,7 @@
 	    $.ajax({
 	        type: "POST",
 	        dataType: "json",
-	        url: "../../contact.php",
+	        url: "contact.php",
 	        data: mydata,
 	        success: function(data) {
 	            if (data.type === "error") {
